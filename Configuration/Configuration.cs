61cdf651fcea9ceb73448958b2d4ca829ca14468
Nosterafi <nosterafi56@gmail.com>
2024-01-19 20:41:56 +0400

    Первая версия

---
+++ Configuration/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingConsultant
+{
+    public static class Configurathion
+    {
+        public static TelegramSettings TelegramSettings { get; set; }
+
+        public static void SetProperities(IConfiguration configurathion)
+        {
+            TelegramSettings = GetProperities<TelegramSettings>(configurathion, "TelegramSettings");
+        }
+
+        public static T GetProperities<T>(IConfiguration configurathion, string sectionName)
+        {
+            return configurathion.GetSection(sectionName).Get<T>()
+                ?? throw new InvalidOperationException($"Not found section {nameof(T)} in configuration.");
+        }
+    }
+}
