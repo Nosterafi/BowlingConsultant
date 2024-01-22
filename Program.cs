55d9b5eed20a7fe8e8020e95116375cd665cf3fe
Nosterafi <nosterafi56@gmail.com>
2024-01-22 19:58:31 +0400

    Удалил Ancwer.cs и исправил код в Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -22,47 +23,43 @@ namespace BowlingConsultant
                 .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables()
                 .Build();
+
             Configurathion.SetProperities(config);
             var _botClient = new TelegramBotClient(Configurathion.TelegramSettings.BotToken);
+
             var _receiverOptions = new ReceiverOptions
             {
                 AllowedUpdates = { },
                 ThrowPendingUpdates = true,
             };
-            using var cts = new CancellationTokenSource();
+            var cts = new CancellationTokenSource();
+
             _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
             var me = await _botClient.GetMeAsync();
+
             Console.WriteLine($"{me.FirstName} started");
-            Console.ForegroundColor = ConsoleColor.Red; //Все дальнейшие собщения в консоли будут красного цвета.
             Console.ReadLine();
         }
 
         private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            switch (update.Type)
+            if (update.Type != UpdateType.Message) 
+                return;
+
+            var message = update.Message;
+
+            if (message.Text == "/start")
             {
-                case UpdateType.Message:
-                    await Answers.AnswerToStart(botClient, update.Message.Chat);
-                    break;
-                case UpdateType.CallbackQuery:
-                    var callbackQuery = update.CallbackQuery;
-                    await botClient.AnswerCallbackQueryAsync(callbackQuery.Id);
-                    await Answers.BotAnswers[callbackQuery.Data](botClient, callbackQuery.Message.Chat);
-                    break;
+                await botClient.SendTextMessageAsync(message.Chat, "Привет. Что вас интерисует?");
+                return;
             }
+
+            await botClient.SendTextMessageAsync(message.Chat, message.Text);
         }
 
         private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
         {
-            var ErrorMessage = error switch
-            {
-                ApiRequestException apiRequestException
-                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                _ => error.ToString()
-            };
-            Console.WriteLine("Было вызвано исключение:");
-            Console.WriteLine(ErrorMessage);
-            Console.WriteLine();
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(error));
             return Task.CompletedTask;
         }
     }
